a671d3e5f572a6dfcc23c43ec3d4a2799a77228c
Aybars Erenkul <90629653+Aybars-Erenkul@users.noreply.github.com>
2023-03-04 14:10:09 +0300

    Added option to import directly from csv file.
    Modified the csv creating function so the gateway information is present. This allows one to create the nodes without having to rescan the network.

--- local_network_mapper.py
+++ local_network_mapper.py
@@ -28,23 +28,34 @@ def arp_scan():
 
         
     for sent, received in result:
-
-        # for each response, append ip and mac address to 'clients' list
-        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
-        #print({'ip': received.psrc, 'mac': received.hwsrc})
+        vendor = mac_details.get_mac_details(received.hwsrc).split()
+        vendor = str(vendor[0])
         if gw == received.psrc:
             gw_mac = received.hwsrc
+            clients.append({'ip': received.psrc, 'mac': received.hwsrc, 'vendor': vendor, 'gateway': 'y'})
+        # for each response, append ip and mac address to 'clients' list
+        else:
+            clients.append({'ip': received.psrc, 'mac': received.hwsrc, 'vendor': vendor, 'gateway': 'n' })
+        #print({'ip': received.psrc, 'mac': received.hwsrc})
+        
 
     print("Available devices in the network:")
-    print("IP" + " "*18+"MAC")
+    print("IP" + " "*18+"MAC"+" "*18+"VENDOR")
     with open("local_network.csv", "w") as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(["IP", "MAC"])
+        writer.writerow(["IP", "MAC", "VENDOR", "GATEWAY"])
         for client in clients:
-            writer.writerow([client['ip'], client['mac']])
-            print("{:16}    {}".format(client['ip'], client['mac']))
+            writer.writerow([client['ip'], client['mac'], client['vendor'], client['gateway']])
+            print("{:16}    {}".format(client['ip'], client['mac'], client["vendor"], client["gateway"]))
 
-arp_scan()
+ask = input("Wanna re-scan? (y/n) : ")
+if(ask == 'y'):
+    arp_scan()
+    gw = conf.route.route("0.0.0.0")[2]
+    gw_vendor = mac_details.get_mac_details(gw_mac).split()
+    gw_vendor = str(gw_vendor[0])
+else:
+    pass
 
 got_data = pd.read_csv("local_network.csv")
 
@@ -52,34 +63,32 @@ net = Network()
 
 print("NOW DISPLAYING GOT DATA")
 print(got_data)
-
-i = 1
 gw = conf.route.route("0.0.0.0")[2]
+i = 1
+
 time.sleep(1)
-gw_vendor = mac_details.get_mac_details(gw_mac).split()
-gw_vendor = str(gw_vendor[0])
+
 
 for i in range(len(got_data)):
     print(got_data['IP'][i])
-    vendor = mac_details.get_mac_details(got_data["MAC"][i]).split()
-    vendor = str(vendor[0])
-    print(vendor)
-    if got_data["IP"][i] == gw:
-        net.add_node(vendor+"\n"+got_data["IP"][i], title=f"MAC: {got_data['MAC'][i]}", color="green")
+    #print(vendor)
+    if got_data["GATEWAY"][i] == 'y':
+        net.add_node(got_data["VENDOR"][i]+"\n"+got_data["IP"][i], title=f"MAC: {got_data['MAC'][i]}", color="green")
     else:
-        net.add_node(vendor+"\n"+got_data["IP"][i], title=f"MAC: {got_data['MAC'][i]}")
-    if got_data["IP"][i] != gw:
-        pass
-        net.add_edge(gw_vendor+"\n"+gw,vendor+"\n"+got_data['IP'][i])
+        net.add_node(got_data["VENDOR"][i]+"\n"+got_data["IP"][i], title=f"MAC: {got_data['MAC'][i]}")
+    if got_data["GATEWAY"][i] != 'y':
+        for k in range(len(got_data)):
+            if got_data["GATEWAY"][k] == 'y':
+                net.add_edge(got_data["VENDOR"][k]+"\n"+got_data["IP"][k],got_data["VENDOR"][i]+"\n"+got_data['IP'][i])
+            else:
+                pass
     else:
         pass
 
 
-    
 
 
 #net.add_edges([(gw,got_data['IP'][1])])
-
 
 
 
