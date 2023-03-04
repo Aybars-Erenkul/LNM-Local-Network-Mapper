d9a34724763981bbdf02c8d2daab6cbbc30d437e
Aybars Erenkul <90629653+Aybars-Erenkul@users.noreply.github.com>
2023-03-04 12:52:05 +0300

    Update local_network_mapper.py

--- local_network_mapper.py
+++ local_network_mapper.py
@@ -3,8 +3,12 @@ from pyvis.network import Network
 from scapy.all import ARP, Ether, srp, conf
 import csv
 import pandas as pd
+import mac_details
+import time
 
 def arp_scan():
+
+    global gw_mac
 
     gw = conf.route.route("0.0.0.0")[2]
 
@@ -28,6 +32,8 @@ def arp_scan():
         # for each response, append ip and mac address to 'clients' list
         clients.append({'ip': received.psrc, 'mac': received.hwsrc})
         #print({'ip': received.psrc, 'mac': received.hwsrc})
+        if gw == received.psrc:
+            gw_mac = received.hwsrc
 
     print("Available devices in the network:")
     print("IP" + " "*18+"MAC")
@@ -49,15 +55,22 @@ print(got_data)
 
 i = 1
 gw = conf.route.route("0.0.0.0")[2]
+time.sleep(1)
+gw_vendor = mac_details.get_mac_details(gw_mac).split()
+gw_vendor = str(gw_vendor[0])
 
 for i in range(len(got_data)):
     print(got_data['IP'][i])
+    vendor = mac_details.get_mac_details(got_data["MAC"][i]).split()
+    vendor = str(vendor[0])
+    print(vendor)
     if got_data["IP"][i] == gw:
-        net.add_node(got_data["IP"][i], title=f"MAC: {got_data['MAC'][i]}", color="green")
+        net.add_node(vendor+"\n"+got_data["IP"][i], title=f"MAC: {got_data['MAC'][i]}", color="green")
     else:
-        net.add_node(got_data["IP"][i], title=f"MAC: {got_data['MAC'][i]}")
+        net.add_node(vendor+"\n"+got_data["IP"][i], title=f"MAC: {got_data['MAC'][i]}")
     if got_data["IP"][i] != gw:
-        net.add_edge(gw,got_data['IP'][i])
+        pass
+        net.add_edge(gw_vendor+"\n"+gw,vendor+"\n"+got_data['IP'][i])
     else:
         pass
 
@@ -65,6 +78,8 @@ for i in range(len(got_data)):
 
 
 #net.add_edges([(gw,got_data['IP'][1])])
+
+
 
 
 net.show('basic.html', notebook=False)
